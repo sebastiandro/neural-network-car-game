00cbe5deaa71cc51368c07f28db766501d8e6d11
Sebastian Nilsson <sebbehebbe@gmail.com>
2017-09-20 17:47:37 +0200

    Genome class

---
+++ Assets/Scripts/Genome.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Genome
+{
+
+	private int score = 0;
+	private NeuralNetwork network;
+
+	public Genome ()
+	{
+	}
+
+	public Genome (NeuralNetwork network, int score)
+	{
+		this.network = network;
+		this.score = score;
+	}
+}
+
