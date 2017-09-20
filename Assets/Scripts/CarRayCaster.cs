c6d01a1e5f149afaa2c2e9153fbe663a77417dd4
Sebastian Nilsson <sebbehebbe@gmail.com>
2017-09-20 16:01:08 +0200

    Output from neural network

--- Assets/Scripts/CarRayCaster.cs
+++ Assets/Scripts/CarRayCaster.cs
@@ -8,14 +8,16 @@ public class CarRayCaster : MonoBehaviour
 	public GameObject car;
 	int rayDistance = 5;
 	int rayDistanceFront = 20;
+	NeuralNetwork neu = new NeuralNetwork ();
 
 	void Start() {
 		Debug.Log ("Start");
 
-		NeuralNetwork neu = new NeuralNetwork ();
-		neu.perceptronGeneration (5, new int[]{4, 3, 6}, 5);
+		//NeuralNetwork neu = new NeuralNetwork ();
+		neu.perceptronGeneration (3, new int[]{4, 3, 6}, 5);
 		List<NeuralNetwork.Layer> layers = neu.getLayers ();
 
+		/*
 		for (int i = 0; i < layers.Count; i++) {
 			NeuralNetwork.Neuron[] layerNeurons = layers [i].getNeurons ();
 			for (int j = 0; j < layerNeurons.Length; j++) {
@@ -27,6 +29,8 @@ public class CarRayCaster : MonoBehaviour
 				}
 			}
 		}
+		*/
+
 	}
 
 	void Update(){
@@ -36,10 +40,16 @@ public class CarRayCaster : MonoBehaviour
 		Ray rightRay = new Ray (car.transform.position, Quaternion.Euler(0, 45, 0) * transform.forward * rayDistance);
 		Ray leftRay = new Ray (car.transform.position, Quaternion.Euler(0, -45, 0) * transform.forward * rayDistance);
 
+		double frontDistance = 0;
+		double leftDistance = 0;
+		double rightDistance = 0;
+
 		// Mark collision from front
 		if (Physics.Raycast(frontRay, out hit, rayDistanceFront)) {
 			Debug.DrawRay(car.transform.position, transform.forward * rayDistanceFront, Color.red);
-			Debug.Log ("Front distance: " + hit.distance);
+			//Debug.Log ("Front distance: " + hit.distance);
+
+			frontDistance = hit.distance / rayDistanceFront;
 		} else {
 			Debug.DrawRay(car.transform.position, transform.forward * rayDistanceFront, Color.green);
 		}
@@ -47,7 +57,9 @@ public class CarRayCaster : MonoBehaviour
 		// Mark collision from right
 		if (Physics.Raycast(rightRay, out hit, rayDistance)) {
 			Debug.DrawRay(car.transform.position, Quaternion.Euler(0, 45, 0) * transform.forward * rayDistance, Color.red);
-			Debug.Log ("Right distance: " + hit.distance);
+			//Debug.Log ("Right distance: " + hit.distance);
+
+			rightDistance = hit.distance / rayDistance;
 		} else {
 			Debug.DrawRay(car.transform.position, Quaternion.Euler(0, 45, 0) * transform.forward * rayDistance, Color.green);
 		}
@@ -55,9 +67,18 @@ public class CarRayCaster : MonoBehaviour
 		// Mark collision from left
 		if (Physics.Raycast(leftRay, out hit, rayDistance)) {
 			Debug.DrawRay(car.transform.position, Quaternion.Euler(0, -45, 0) * transform.forward * rayDistance, Color.red);
-			Debug.Log ("Left distance: " + hit.distance);
+			//Debug.Log ("Left distance: " + hit.distance);
+			leftDistance = hit.distance / rayDistance;
 		} else {
 			Debug.DrawRay(car.transform.position, Quaternion.Euler(0, -45, 0) * transform.forward * rayDistance, Color.green);
+		}
+
+		print ("Left distance: " + leftDistance + " Front Distance: " + frontDistance + " Right Distance: " + rightDistance);
+
+		double[] output = neu.compute (new double[]{ leftDistance, frontDistance, rightDistance });
+
+		for (int i = 0; i < output.Length; i++) {
+			Debug.Log ("Output " + i + ": " + output[i]);
 		}
 
 	}
--- Assets/Scripts/NeuralNetwork.cs
+++ Assets/Scripts/NeuralNetwork.cs
@@ -89,12 +89,73 @@ public class NeuralNetwork {
 		return weights;
 	}
 
+	public void setNeuronsAndWeights(int[] nbNeuronsPerLayer, double[][] weights){
+		int previousNeurons = 0;
+		int index = 0;
+		int indexWeights = 0;
+
+		layers = new List<Layer> ();
+
+		for (int i = 0; i < nbNeuronsPerLayer.Length; i++) {
+			Layer layer = new Layer (i);
+			layer.populate (nbNeuronsPerLayer [i], previousNeurons);
+			Neuron[] neurons = layer.getNeurons ();
+
+			for (int j = 0; j < neurons.Length; j++) {
+				neurons [j].setWeights (weights [indexWeights]);
+			}
+
+			previousNeurons = nbNeuronsPerLayer [i];
+		}
+
+	}
+
+	public double[] compute(double[] inputs){
+
+		Layer inputLayer = this.layers[0];
+		Neuron[] inputLayerNeurons = inputLayer.getNeurons ();
+
+		// Set values for input layer
+		for (int i = 0; i < inputs.Length; i++) {
+			inputLayerNeurons [i].setValue (inputs [i]);
+		}
+
+		Layer prevLayer = inputLayer;
+		for (int j = 1; j < this.layers.Count; j++) {
+
+			Neuron[] layerNeurons = this.layers [j].getNeurons();
+
+			for (int k = 0; k < layerNeurons.Length; k++) {
+
+				double sum = 0;
+
+				for (int l = 0; l < prevLayer.getNeurons ().Length; l++) {
+					sum += prevLayer.getNeurons () [l].getValue () * layerNeurons [k].getWeights () [l];
+				}
+
+				layerNeurons [k].setValue (activation (sum));
+			}
+
+			prevLayer = this.layers [j];
+		}
+			
+		Layer lastLayer = layers [layers.Count - 1];
+		double[] output = new double[lastLayer.getNeurons ().Length];
+
+		for (int m = 0; m < lastLayer.getNeurons ().Length; m++) {
+			output [m] = lastLayer.getNeurons () [m].getValue ();
+		}
+
+		return output;
+
+	}
+
 	public List<Layer> getLayers() {
 		return layers;
 	}
 		
 	public class Neuron {
-		private int value = 0;
+		private double value = 0;
 		private double[] weights;
 
 		public void populate(int nb){
@@ -106,6 +167,18 @@ public class NeuralNetwork {
 
 		public double[] getWeights() {
 			return weights;
+		}
+
+		public void setWeights(double[] weights) {
+			this.weights = weights;
+		}
+
+		public void setValue(double value) {
+			this.value = value;
+		}
+
+		public double getValue() {
+			return value;
 		}
 
 	}
